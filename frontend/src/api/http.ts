03eaa2554d67aeb9c7941e87b9484a72908d5ad4
Jakub Szafrański <s@samu.pl>
2020-11-28 02:21:57 +0100

    Docker improvements

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+const baseURL = process.env.VUE_APP_API_URL || "http://127.0.0.1:8080/"
 export default axios.create({
-  baseURL: "http://192.168.1.228:8888/", // TODO: duh.
+  baseURL: baseURL, // TODO: duh.
   headers: {
     "Content-type": "application/json"
   }
